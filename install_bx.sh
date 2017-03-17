435cf2f51b0829588b4e99a4d3cc15d53a9d4037
Adam King <rak@us.ibm.com>
2017-03-17 08:57:42 -0400

    Install cs plugin
    Signed-off-by: Adam King <rak@us.ibm.com>

--- install_bx.sh
+++ install_bx.sh
@@ -13,7 +13,7 @@ export PATH="/tmp/Bluemix_CLI/bin:$PATH"
 
 # Install Armada CS plugin
 echo "Install the Bluemix container-service plugin"
-#bx plugin install bin/container-service-linux-amd64
+bx plugin install container-service -r Bluemix
 
 echo "Install kubectl"
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
