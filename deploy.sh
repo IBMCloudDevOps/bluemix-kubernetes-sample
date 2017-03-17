b0060be37345833fb347949d639ff3a03c3bea90
Adam King <rak@us.ibm.com>
2017-03-17 10:20:07 -0400

    Remove sudo
    Signed-off-by: Adam King <rak@us.ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 echo "Create Guestbook"
-IP_ADDR=$(sudo bx cs workers $CLUSTER_NAME | grep deployed | awk '{ print $2 }')
+IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep deployed | awk '{ print $2 }')
 
 echo -e "\tConfiguring vars"
-$(sudo bx cs cluster-config $CLUSTER_NAME | grep export)
+$(bx cs cluster-config $CLUSTER_NAME | grep export)
 
 echo -e "\tDownlodaing guestbook yml"
 curl "https://raw.githubusercontent.com/kubernetes/kubernetes/master/examples/guestbook/all-in-one/guestbook-all-in-one.yaml" > guestbook.yml
 sed -i '130i\ \ type: NodePort' guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
 
 echo -e "\tChecking if previous version of guestbook exists"
-sudo kubectl get services | grep frontend > /dev/null
+kubectl get services | grep frontend > /dev/null
 RET=$?
 
 #Service exists
 if [ $RET -eq 0 ]; then
   echo -e "\tDeleting previous version of guestbook"
-  sudo kubectl delete -f guestbook.yml
+  kubectl delete -f guestbook.yml
 fi
 
 echo -e "\tCreating guestbook"
-sudo kubectl create -f guestbook.yml
+kubectl create -f guestbook.yml
 
-PORT=$(sudo kubectl get services | grep frontend | sed 's/.*://g' | sed 's/\/.*//g')
+PORT=$(kubectl get services | grep frontend | sed 's/.*://g' | sed 's/\/.*//g')
 
 echo ""
 echo "View the guestbook at http://$IP_ADDR:$PORT"
