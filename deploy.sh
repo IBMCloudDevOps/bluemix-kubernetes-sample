dfa51a11ef532556c44549a144edd4db287b9fa2
Adam King <rak@us.ibm.com>
2017-03-17 09:49:41 -0400

    Use predefined env vars. Exit early if reqiured vars are not set.
    Signed-off-by: Adam King <rak@us.ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
+  echo "Define all required environment variables and rerun the stage."
+  exit 1
+fi
 echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$BLUEMIX_ORGANIZATION" -s "$BLUEMIX_SPACE"
+bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
 
 # Init container clusters
 echo "bx cs init"
