8dbfa6036fef23c7f806f32bc6438661f3240934
Adam King <rak@us.ibm.com>
2017-03-17 09:21:52 -0400

    Require org and space
    Signed-off-by: Adam King <rak@us.ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT"
+bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$BLUEMIX_ORGANIZATION" -s "$BLUEMIX_SPACE"
 
 # Init container clusters
-#echo "bx cs init"
-#bx cs init -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD"
+echo "bx cs init"
+bx cs init -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD"
