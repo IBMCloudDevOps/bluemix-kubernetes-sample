a20430444687ca9a775506b41c48b43f27ab642d
Adam King <rak@us.ibm.com>
2017-03-20 11:45:51 -0400

    Eror handling for sh scripts
    Signed-off-by: Adam King <rak@us.ibm.com>

--- bx_login.sh
+++ bx_login.sh
@@ -8,7 +8,15 @@ echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
 bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+if [ $? -ne 0 ]; then
+  echo "Failed to authenticate to Bluemix"
+  exit 1
+fi
 
 # Init container clusters
 echo "bx cs init"
 bx cs init -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD"
+if [ $? -ne 0 ]; then
+  echo "Failed to authenticate to Bluemix Container Service"
+  exit 1
+fi
