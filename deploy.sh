3dd2952751475a766ce1f53e4e24de65b4f5b19d
Adam King <rak@linux.vnet.ibm.com>
2017-03-22 18:49:23 -0400

    Always delete pods before deploying. (#3)
    * Always delete the pods when deploying.
    
    Signed-off-by: Adam King <rak@us.ibm.com>
    
    * Update README.md

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "Create Guestbook"
 IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep deployed | awk '{ print $2 }')
 if [ -z $IP_ADDR ]; then
-  echo "$CLUSTER_NAME workers not ready"
+  echo "$CLUSTER_NAME not created or workers not ready"
   exit 1
 fi
 
@@ -19,17 +19,10 @@ echo -e "Downloading guestbook yml"
 curl --silent "https://raw.githubusercontent.com/kubernetes/kubernetes/master/examples/guestbook/all-in-one/guestbook-all-in-one.yaml" > guestbook.yml
 sed -i '130i\ \ type: NodePort' guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
 
-echo -e "Checking if previous version of guestbook exists"
-kubectl get services | grep frontend > /dev/null
-RET=$?
+echo -e "Deleting previous version of guestbook if it exists"
+kubectl delete --ignore-not-found=true   -f guestbook.yml
 
-#Service exists
-if [ $RET -eq 0 ]; then
-  echo -e "Deleting previous version of guestbook"
-  kubectl delete -f guestbook.yml
-fi
-
-echo -e "Creating guestbook"
+echo -e "Creating pods"
 kubectl create -f guestbook.yml
 
 PORT=$(kubectl get services | grep frontend | sed 's/.*://g' | sed 's/\/.*//g')
