939cd956d6713f3b68e5cc714803d6cbde1d924c
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-05-04 08:29:14 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ curl --silent "https://raw.githubusercontent.com/kubernetes/kubernetes/master/ex
 #Find the line that has the comment about the load balancer and add the nodeport def after this
 let NU=$(awk '/^  # type: LoadBalancer/{ print NR; exit }' guestbook.yml)+3
 NU=$NU\i
-sed -i '$NU\ \ type: NodePort' guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
+sed -i "$NU\ \ type: NodePort" guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
 
 echo -e "Deleting previous version of guestbook if it exists"
 kubectl delete --ignore-not-found=true   -f guestbook.yml
